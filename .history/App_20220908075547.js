79189eea20c0a2d32dc9d4bc1db8602ab9115ea8
MhlengiNgubane <mhlengims@gmail.com>
2022-09-09 10:38:22 +0200

    almost

--- .history/App_20220908075547.js
+++
@@ -1,8 +0,0 @@
-let displayValue = '0';
-let firstOperand = null;
-let secondOperand = null;
-let firstOperator = null;
-let secondOperator = null;
-let result = null;
-const buttons = document.querySelectorAll('button');
-
